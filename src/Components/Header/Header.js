858794fbfacd736a549177d204a68a30b410e693
juzQrios <dj30c.1@gmail.com>
2020-11-03 13:36:15 +0530

    Add Header Component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+
+const Header = () => (
+  <header className="Header">
+    <h1><a href="https://www.freecodecamp.org/" target="blank">freeCodeCamp</a> Projects</h1>
+  </header>
+);
+
+export default Header;
