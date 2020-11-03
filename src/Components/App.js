648f135caea085639260a20779a9824eb5ff8d4f
juzQrios <dj30c.1@gmail.com>
2020-11-03 13:35:20 +0530

    Move App component

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header/Header';
+import CardGenerator from './CardGenerator/CardGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CardGenerator />
+    </div>
+  );
+}
+
+export default App;
